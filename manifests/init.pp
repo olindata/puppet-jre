c599fcd7b79ff151319abdcb6a7fceceab499529
Walter Heck <walterheck@olindata.com>
2012-07-08 12:03:58 +0800

    Initial version of jre including debconf preseeds in order to satisfy the licenseagreement questions

---
+++ manifests/init.pp
@@ -0,0 +1,14 @@
+class jre {
+
+  package { 'sun-java6-jre':
+    ensure       => latest,
+    require      => File['/var/cache/debconf/jre6.seeds'],
+    responsefile => '/var/cache/debconf/jre6.seeds',
+  }
+
+  file { '/var/cache/debconf/jre6.seeds':
+    ensure => present,
+    source => 'puppet:///jre/jre6.seeds',
+  }
+
+}
